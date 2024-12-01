27163879d6257bcab68d48ae9490f39208c7c119
Jakob Beckmann <f4z3r-github@pm.me>
2024-12-01 17:24:48 +0100

    feat(jbe): add day 01 solutions

---
+++ 2024/JBE/README.md
@@ -0,0 +1,13 @@
+# JBE's Solutions to AOC 2024
+
+Run tests based on examples provided:
+
+```sh
+cargo test
+```
+
+Run a solution:
+
+```sh
+cargo run --bin day-xx
+```
---
+++ 2024/JBE/day-01/src/main.rs
@@ -0,0 +1,92 @@
+use std::collections::HashMap;
+use std::fs;
+
+use std::str::FromStr;
+
+type Result<T> = ::std::result::Result<T, Box<dyn ::std::error::Error>>;
+
+fn main() -> Result<()> {
+    let content = fs::read_to_string("day-01/input.txt")?;
+    let lines = parse(&content);
+    let res_1 = part_1(&lines)?;
+    let res_2 = part_2(&lines)?;
+    println!("Part 1: {}", res_1);
+    println!("Part 2: {}", res_2);
+    Ok(())
+}
+
+fn part_1(lines: &[Line]) -> Result<u32> {
+    let mut first = Vec::new();
+    let mut second = Vec::new();
+    for line in lines {
+        first.push(line.first);
+        second.push(line.second);
+    }
+    first.sort();
+    second.sort();
+    let distance = first
+        .iter()
+        .zip(second.iter())
+        .fold(0, |acc, (f, s)| acc + f.abs_diff(*s));
+    Ok(distance)
+}
+
+fn part_2(lines: &[Line]) -> Result<u32> {
+    let mut first = Vec::new();
+    let mut second = HashMap::new();
+    for line in lines {
+        first.push(line.first);
+        if let Some(x) = second.get_mut(&line.second) {
+            *x = *x + 1;
+        } else {
+            second.insert(&line.second, 1);
+        }
+    }
+    first
+        .iter()
+        .map(|e| e * second.get(&e).unwrap_or(&0))
+        .reduce(|acc, e| acc + e)
+        .ok_or("no input".into())
+}
+
+fn parse(content: &str) -> Vec<Line> {
+    content.lines().map(|l| l.parse().unwrap()).collect()
+}
+
+#[derive(Debug)]
+struct Line {
+    first: u32,
+    second: u32,
+}
+
+impl FromStr for Line {
+    type Err = Box<dyn ::std::error::Error>;
+
+    fn from_str(s: &str) -> Result<Line> {
+        let mut parts = s.split("   ");
+        Ok(Line {
+            first: parts
+                .next()
+                .ok_or("failed to find first element")?
+                .parse()?,
+            second: parts
+                .next()
+                .ok_or("failed to find second element")?
+                .parse()?,
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::*;
+
+    #[test]
+    fn test_example() -> Result<()> {
+        let content = fs::read_to_string("example.txt")?;
+        let lines = parse(&content);
+        let res = part_1(&lines)?;
+        assert_eq!(11, res);
+        Ok(())
+    }
+}
