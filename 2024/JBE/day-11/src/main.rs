353ed171b03e4d20c5cf92baa86f6b4e6a517057
Jakob Beckmann <f4z3r-github@pm.me>
2024-12-23 13:45:02 +0100

    feat(jbe): update day 11 without pointer accumulator

--- 2024/JBE/day-11/src/main.rs
+++ 2024/JBE/day-11/src/main.rs
@@ -1,4 +1,5 @@
-use std::{collections::HashMap, fs};
+use std::collections::HashMap;
+use std::fs;
 
 type Result<T> = ::std::result::Result<T, Box<dyn ::std::error::Error>>;
 
@@ -13,54 +14,48 @@ fn main() -> Result<()> {
 }
 
 fn part_1(stones: &[usize]) -> Result<usize> {
-    let mut sum = stones.len();
     let mut cache = HashMap::new();
-    for stone in stones {
-        split(*stone, 0, &mut sum, &mut cache, 25)?;
-    }
-    Ok(sum)
+    stones
+        .iter()
+        .map(|stone| split(*stone, 0, &mut cache, 25))
+        .sum()
 }
 
 fn part_2(stones: &[usize]) -> Result<usize> {
-    let mut sum = stones.len();
     let mut cache = HashMap::new();
-    for stone in stones {
-        split(*stone, 0, &mut sum, &mut cache, 75)?;
-    }
-    Ok(sum)
+    stones
+        .iter()
+        .map(|stone| split(*stone, 0, &mut cache, 75))
+        .sum()
 }
 
 fn split(
     num: usize,
     level: usize,
-    acc: &mut usize,
     cache: &mut HashMap<usize, HashMap<usize, usize>>,
     stop: usize,
-) -> Result<()> {
+) -> Result<usize> {
     if level == stop {
-        return Ok(());
+        return Ok(1);
     }
     if let Some(num_cache) = cache.get(&num) {
         if let Some(val) = num_cache.get(&level) {
-            *acc = *acc + val;
-            return Ok(());
+            return Ok(*val);
         }
     }
-    let before = *acc;
     let string = num.to_string();
-    match string.as_str() {
-        "0" => split(1, level + 1, acc, cache, stop)?,
+    let res = match string.as_str() {
+        "0" => split(1, level + 1, cache, stop)?,
         x if x.len() % 2 == 0 => {
             let (first, second) = string.split_at(string.len() / 2);
-            *acc = *acc + 1;
-            split(first.parse()?, level + 1, acc, cache, stop)?;
-            split(second.parse()?, level + 1, acc, cache, stop)?;
+            split(first.parse()?, level + 1, cache, stop)?
+                + split(second.parse()?, level + 1, cache, stop)?
         }
-        _ => split(num * 2024, level + 1, acc, cache, stop)?,
-    }
+        _ => split(num * 2024, level + 1, cache, stop)?,
+    };
     let num_cache = cache.entry(num).or_insert_with(|| HashMap::new());
-    num_cache.insert(level, *acc - before);
-    Ok(())
+    num_cache.insert(level, res);
+    Ok(res)
 }
 
 fn parse(content: &str) -> ::std::result::Result<Vec<usize>, ::std::num::ParseIntError> {
