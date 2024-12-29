7772287eaf7380d7f96ee1b87d3a261739ab020e
Weili Gao <9134665+weiligao@users.noreply.github.com>
2024-12-29 12:25:13 +0100

    feat(wga): day 17 part 2

--- 2024/WGA/17/aoc_template.py
+++ 2024/WGA/17/aoc_template.py
@@ -4,6 +4,21 @@ import pathlib
 import sys
 import re
 
+def get_a(program, cursor, a):
+    for i in range(8):
+        new_a = (a << 3) + i
+
+        if run_program(program, new_a, 0, 0) == program[cursor:]:
+            if cursor == 0:
+                return new_a
+            
+            result = get_a(program, cursor-1, new_a)
+
+            if result is not None:
+                return result
+            
+    return None
+
 def combo(operand, a, b, c):
     if operand < 4:
         return operand
@@ -16,7 +31,7 @@ def combo(operand, a, b, c):
     
     raise ValueError("Invalid combo operand")
 
-def run_program(program, register_a, register_b, register_c):
+def run_program(program, a, b, c):
     pointer = 0
     output = []
 
@@ -24,45 +39,48 @@ def run_program(program, register_a, register_b, register_c):
         instruction = program[pointer]
         operand = program[pointer+1]
 
-        if instruction == 0: # adv
-            register_a = register_a >> combo(operand, register_a, register_b, register_c)
-        elif instruction == 1: # bxl
-            register_b ^= operand
-        elif instruction == 2: # bst
-            register_b = combo(operand, register_a, register_b, register_c) % 8
-        elif instruction == 3: # jnz
-            if register_a > 0:
+        if instruction == 0:
+            a >>= combo(operand, a, b, c)
+        elif instruction == 1:
+            b ^= operand
+        elif instruction == 2:
+            b = combo(operand, a, b, c) % 8
+        elif instruction == 3:
+            if a > 0:
                 pointer = operand
                 continue
-        elif instruction == 4: # bxc
-            register_b ^= register_c
-        elif instruction == 5: # out
-            output.append(str(combo(operand, register_a, register_b, register_c) % 8))
-        elif instruction == 6: # bdv
-            register_b = register_a >> combo(operand, register_a, register_b, register_c)
-        else: # cdv
-            register_c = register_a >> combo(operand, register_a, register_b, register_c)
+        elif instruction == 4:
+            b ^= c
+        elif instruction == 5:
+            output.append(combo(operand, a, b, c) % 8)
+        elif instruction == 6:
+            b = a >> combo(operand, a, b, c)
+        else:
+            c = a >> combo(operand, a, b, c)
             
         pointer += 2
 
-    return ",".join(output)
+    return output
 
 def parse(puzzle_input):
     """Parse input."""
     registers, program = puzzle_input.split("\n\n")
-    register_a, register_b, register_c = list(map(int, re.findall(r"\d+", registers)))
+    a, b, c = list(map(int, re.findall(r"\d+", registers)))
     program = list(map(int, re.findall(r"[0-7]", program)))
 
-    return register_a, register_b, register_c, program
+    return a, b, c, program
 
 def part1(data):
     """Solve part 1."""
-    register_a, register_b, register_c, program = data
+    a, b, c, program = data
 
-    return run_program(program, register_a, register_b, register_c)
+    return ",".join([str(value) for value in run_program(program, a, b, c)])
 
 def part2(data):
     """Solve part 2."""
+    program = data[3]
+
+    return get_a(program, len(program)-1, 0)
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
