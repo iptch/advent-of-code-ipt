449831a1b5cbef704a6f22098283678837c7a455
Jakob Beckmann <f4z3r-github@pm.me>
2025-11-29 17:55:26 +0100

    feat(jbe): prepare lua styling config
    Signed-off-by: Jakob Beckmann <f4z3r-github@pm.me>

---
+++ 2025/JBE/day-01/solve.lua
@@ -0,0 +1,16 @@
+local function part_1()
+  return 0
+end
+
+local function part_2()
+  return 0
+end
+
+local function main()
+  local p1 = part_1()
+  print("Part 1:", p1)
+  local p2 = part_2()
+  print("Part 2:", p2)
+end
+
+main()
