3455899ee039a9868963ffdcaa8610cde06fb5a4
Jakob Beckmann <f4z3r-github@pm.me>
2025-12-01 07:27:21 +0100

    feat(jbe): day 1 solutions
    Signed-off-by: Jakob Beckmann <f4z3r-github@pm.me>

---
+++ 2025/JBE/README.md
@@ -0,0 +1,10 @@
+# JBE's Solutions to AOC 2025
+
+Run the solutions as follows within each directory:
+
+```sh
+# run on test input based on example.txt
+luajit solve.lua test
+# run on actual input based on input.txt
+luajit solve.lua
+```
--- 2025/JBE/day-01/solve.lua
+++ 2025/JBE/day-01/solve.lua
@@ -1,9 +1,48 @@
+local io = require("io")
+local math = require("math")
+
+local inputfile = arg[1] == "test" and "example.txt" or "input.txt"
+
 local function part_1()
-  return 0
+  local dial = 50
+  local res = 0
+  for line in io.lines(inputfile) do
+    local val = line:match("L(%d+)")
+    if val then
+      val = -tonumber(val)
+    else
+      val = tonumber(line:match("R(%d+)"))
+    end
+    dial = (dial + val) % 100
+    if dial == 0 then
+      res = res + 1
+    end
+  end
+  return res
 end
 
 local function part_2()
-  return 0
+  local dial = 50
+  local res = 0
+  for line in io.lines(inputfile) do
+    local val = line:match("L(%d+)")
+    if val then
+      val = -tonumber(val)
+    else
+      val = tonumber(line:match("R(%d+)"))
+    end
+    assert(val, "should parse number")
+    local rounds = math.floor(math.abs(val) / 100)
+    res = res + rounds
+    local rem = math.fmod(val, 100)
+    local old = dial
+    dial = dial + rem
+    if old ~= 0 and (dial > 99 or dial < 1) then
+      res = res + 1
+    end
+    dial = dial % 100
+  end
+  return res
 end
 
 local function main()
