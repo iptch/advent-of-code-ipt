c936e081d08697590a7367ca9a23a3e77f1bd565
Mathis Dedial <mathis@dedial.net>
2025-12-02 09:57:23 +0100

    MDE Day 02

---
+++ 2025/MDE/day_02_1/src/main.rs
@@ -0,0 +1,52 @@
+#[derive(Debug)]
+struct Range {
+    from: u64,
+    to: u64,
+}
+
+fn is_invalid(n: u64) -> bool {
+    let n_str = n.to_string();
+
+    // Must have even length
+    if n_str.len() % 2 == 1 {
+        return false;
+    }
+
+    let left = &n_str[0..n_str.len() / 2];
+    let right = &n_str[n_str.len() / 2..n_str.len()];
+
+    left.eq(right)
+}
+
+fn sum_invalid_ids(range: &Range) -> u64 {
+    let mut sum = 0;
+    for n in range.from..range.to + 1 {
+        if is_invalid(n) {
+            sum += n
+        }
+    }
+
+    sum
+}
+
+fn parse_line(line: &String) {
+    let str_ranges: Vec<&str> = line.split(",").collect();
+    println!("str_ranges: {:?}", str_ranges);
+    let mut invalid_id_sum = 0;
+    for str_range in str_ranges {
+        let range_limits: Vec<&str> = str_range.split("-").collect();
+        let range = Range {
+            from: range_limits[0].parse().unwrap(),
+            to: range_limits[1].parse().unwrap(),
+        };
+        println!("range: {:?}", range);
+        invalid_id_sum += sum_invalid_ids(&range);
+    }
+    println!("Sum of invalid IDs: {}", invalid_id_sum);
+}
+
+fn main() {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    parse_line(&input);
+}
