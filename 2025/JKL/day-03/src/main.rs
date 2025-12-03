149b62155a773aeee8949a7b854b98a6ad71585e
Jan Kleine <janpaulkleine@icloud.com>
2025-12-03 08:09:46 +0100

    feat(jkl): add solution for day 03

---
+++ 2025/JKL/day-03/src/main.rs
@@ -0,0 +1,83 @@
+use std::str::FromStr;
+
+#[derive(Debug)]
+struct Input {
+    banks: Vec<Vec<u32>>,
+}
+
+impl FromStr for Input {
+    type Err = std::num::ParseIntError;
+    fn from_str(raw_input: &str) -> Result<Self, Self::Err> {
+        Ok(Input {
+            banks: raw_input
+                .lines()
+                .map(|line| line.chars().map(|c| c.to_digit(10).unwrap()).collect())
+                .collect(),
+        })
+    }
+}
+
+fn main() {
+    let input = include_str!("../input.txt");
+    let answer = puzzle_1(input.parse().unwrap());
+    println!("The solution to part 1 is '{}'", answer);
+    let answer = puzzle_2(input.parse().unwrap());
+    println!("The solution to part 2 is '{}'", answer);
+}
+
+fn puzzle_1(input: Input) -> u32 {
+    input.banks.iter().map(|bank| find_max_in_bank(bank)).sum()
+}
+
+fn find_max_in_bank(bank: &Vec<u32>) -> u32 {
+    let max = bank[..bank.len() - 1].iter().max().unwrap();
+    let max_id = bank[..bank.len() - 1]
+        .iter()
+        .enumerate()
+        .find(|(_, val)| *val == max)
+        .unwrap()
+        .0;
+
+    let second_max = bank[max_id + 1..].iter().max().unwrap();
+
+    max * 10 + second_max
+}
+
+fn puzzle_2(input: Input) -> u64 {
+    input
+        .banks
+        .iter()
+        .map(|bank| find_max_in_bank_dp(bank))
+        .sum()
+}
+
+fn find_max_in_bank_dp(bank: &Vec<u32>) -> u64 {
+    let mut dp = vec![vec![0_u64; bank.len() + 1]; 13];
+
+    for i in 1..=12 {
+        for j in 1..=bank.len() {
+            dp[i][j] = u64::max(dp[i][j - 1], dp[i - 1][j - 1] * 10 + bank[j - 1] as u64)
+        }
+    }
+
+    dp[12][bank.len()]
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{puzzle_1, puzzle_2};
+
+    static INPUT: &str = include_str!("../example.txt");
+
+    #[test]
+    fn test_puzzle_1() {
+        let answer = puzzle_1(INPUT.parse().unwrap());
+        assert_eq!(answer, 357);
+    }
+
+    #[test]
+    fn test_puzzle_2() {
+        let answer = puzzle_2(INPUT.parse().unwrap());
+        assert_eq!(answer, 3121910778619);
+    }
+}
