69b011914f13ee3cf940023e8936c2541d1db8ed
Manuel Grossmann <manuel.grossmann@ipt.ch>
2025-12-11 08:14:54 +0100

    MGR prep day12

---
+++ 2025/MGR/day12/solution.py
@@ -0,0 +1,9 @@
+import sys, math
+from collections import defaultdict, deque
+
+file = open("/home/manuelgr/advent-of-code-ipt/2025/MGR/day12/input.txt", "r")
+
+input = file.read().splitlines()
+file.close()
+
+
